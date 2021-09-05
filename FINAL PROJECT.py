df57867adfa1498006852c5e458ead5f87a729d6
jazztutt <67172628+jazztutt@users.noreply.github.com>
2021-09-05 13:14:04 +0100

    Update FINAL PROJECT.py

--- FINAL PROJECT.py
+++ FINAL PROJECT.py
@@ -1,34 +1,110 @@
 import csv
 
-def read_data():
-    data=[]
+print('2018 Sales and Expenditure Analysis')
+print()
 
-    with open('Sales.csv','r')as sales_file:
-        monthly_sales= csv.DictReader(sales_file)
-        for rows in monthly_sales:
-            data.append(rows)
+# Code to read sales data from a csv file
+
+def sales_data():
+
+    data = []
+
+    with open('sales.csv', 'r') as sales_csv:
+        spreadsheet = csv.DictReader(sales_csv)
+        for row in spreadsheet:
+            data.append(row)
 
     return data
 
-def run():
-    data = read_data()
+# Code to analyse sales data
 
-    sales=[]
-    for row in data:
-        sale = int(row['sales'])
+def summary():
+
+    data = sales_data()
+
+    sales = []
+
+    for column in data:
+        sale = int(column['sales'])
         sales.append(sale)
 
-        total = sum(sales)
-        by_month = (row['month'])
-        by_year = (row['year'])
+    expenditure = []
 
-    print('Total Sales {} {}: {}'.format(by_month, by_year, total))
+    for column in data:
+        expense = int(column['expenditure'])
+        expenditure.append(expense)
 
-run()
+    months = []
+
+    for row in data:
+        month = row ['month']
+        months.append(month)
 
 
+    total_sales = sum(sales)
+    average_sales= round(sum(sales)/12)
+    total_expense = sum(expenditure)
+    average_expense = round(sum(expenditure)/12)
+
+    highest_salesmonth = months[sales.index(max(sales))]
+    highest_salesvalue = max(sales)
+    lowest_salesmonth = months[sales.index(min(sales))]
+    lowest_salesvalue = min(sales)
+
+    highest_expendmonth = months[expenditure.index(max(expenditure))]
+    highest_expendvalue = max(expenditure)
+    lowest_expendmonth =  months[expenditure.index(min(expenditure))]
+    lowest_expendvalue =  min(expenditure)
 
 
+    print('Total Sales = £{}'.format(total_sales))
+    print('Average Sales Per Month = £{}' .format(average_sales))
+    print('Highest sales month: {} £{}'.format(highest_salesmonth,highest_salesvalue))
+    print('Lowest sales month: {} £{}'.format(lowest_salesmonth,lowest_salesvalue))
+
+    print()
+    print('Total Expenditure = £{}'.format(total_expense))
+    print('Average Expenditure Per Month = £{}'.format(average_expense))
+    print('Highest Expenditure Month: {} £{}'.format(highest_expendmonth,highest_expendvalue))
+    print('Lowest Expenditure Month: {} £{}'. format(lowest_expendmonth,lowest_expendvalue))
+
+summary()
+
+# Code to ask the user to input a month and receive sales data
+
+def question():
+
+    data = sales_data()
+
+    sales = []
+
+    for column in data:
+        sale = int(column['sales'])
+        sales.append(sale)
+
+    expenditure = []
+
+    for column in data:
+        expense = int(column['expenditure'])
+        expenditure.append(expense)
+
+    months = []
+
+    for row in data:
+        month = row['month']
+        months.append(month)
 
 
+    while True:
+        print()
+        request = input('What month would you like sales data on?').capitalize()
 
+        if request not in months:
+            print('Invalid: Please check your spelling.')
+
+        else:
+            print('Total Sales {}: £'.format(request), (sales[months.index(request)]))
+            print('Total Expenditure {}: £'.format(request), (expenditure[months.index(request)]))
+            break
+
+question()
